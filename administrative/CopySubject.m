76ca874ee1d4657bb0bebbe26f30b18580fb09c3
pinheirochagas <ppinheirochagas@gmail.com>
2018-08-13 12:33:22 -0700

    initial

---
+++ administrative/CopySubject.m
@@ -0,0 +1,27 @@
+function CopySubject(subj_name, s_psychData, d_psychData, s_neuralData, d_neuralData)
+% s source 
+% d destination
+
+%% psychData
+if ~exist([d_psychData subj_name])
+    copyfile([s_psychData '/' subj_name], [d_psychData '/' subj_name])
+else
+end
+
+%% neuralData
+neuralData_folders = {'originalData', 'CARData', 'CompData', 'FiltData', ...
+    'SpecData', 'HFBData'};
+
+for i = 1:length(neuralData_folders)
+    
+    if ~exist([d_psychData '/' neuralData_folders{i} '/' subj_name])
+        disp(['copying ' neuralData_folders{i}])
+        copyfile([s_neuralData '/' neuralData_folders{i} '/' subj_name], [d_neuralData '/' neuralData_folders{i} '/' subj_name])
+    else
+    end
+    
+end
+
+
+end
+
